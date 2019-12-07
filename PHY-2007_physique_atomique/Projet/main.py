7aa2830c04e3f1210272f6adad49ea82e6e600e4
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2019-12-07 13:41:59 -0500

    Update main.py

--- PHY-2007_physique_atomique/Projet/main.py
+++ PHY-2007_physique_atomique/Projet/main.py
@@ -1,17 +1,12 @@
-from QuantumState import QuantumState
-from Transition import Transition
-
-
-def get_spontanious_decay_mean(n, n_prime):
-    transitions = QuantumState.get_valid_transitions_n_to_n(n, n_prime)
-    print(transitions)
-    decay_sum = 0
-    for transition in transitions:
-        rs = transition.get_spontaniuous_decay_rate()
-        print(f"R^s = {rs}")
-        decay_sum += rs
-    return decay_sum/len(transitions)
+from Transitions import Transitions
+import Constantes as const
 
 
 if __name__ == '__main__':
-    get_spontanious_decay_mean(2, 1)
+    rs_mean_normalized_coeff = ((const.alpha**5)*const.mu0*(const.c**2))/const.hbar
+    omega_normalized_coeff = ((const.alpha ** 2) * const.mu0 * (const.c ** 2)) / (2*const.hbar)
+
+    transitions_2_to_1 = Transitions(n=2, n_prime=1)
+    print(transitions_2_to_1)
+    print(f"R^s_mean / rs_mean_normalized_coeff  ="
+          f" {transitions_2_to_1.get_spontanious_decay_mean()/rs_mean_normalized_coeff}")
