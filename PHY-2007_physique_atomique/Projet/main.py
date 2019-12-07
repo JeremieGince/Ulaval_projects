9cf6890b64d90d4096ae9339b55bbabe3610afae
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2019-12-07 00:26:58 -0500

    Update main.py

--- PHY-2007_physique_atomique/Projet/main.py
+++ PHY-2007_physique_atomique/Projet/main.py
@@ -1,4 +1,17 @@
+from QuantumState import QuantumState
+from Transition import Transition
+
+
+def get_spontanious_decay_mean(n, n_prime):
+    transitions = QuantumState.get_valid_transitions_n_to_n(n, n_prime)
+    print(transitions)
+    decay_sum = 0
+    for transition in transitions:
+        rs = transition.get_spontaniuous_decay_rate()
+        print(f"R^s = {rs}")
+        decay_sum += rs
+    return decay_sum/len(transitions)
 
 
 if __name__ == '__main__':
-    pass
+    get_spontanious_decay_mean(2, 1)
