98232c0914a646682c6871b1b9ca3728b4b2fc72
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2019-12-07 15:09:17 -0500

    Update main.py

--- PHY-2007_physique_atomique/Projet/main.py
+++ PHY-2007_physique_atomique/Projet/main.py
@@ -6,9 +6,10 @@ if __name__ == '__main__':
     rs_mean_normalized_coeff = ((const.alpha**5)*const.mu0*(const.c**2))/const.hbar
     omega_normalized_coeff = ((const.alpha ** 2) * const.mu0 * (const.c ** 2)) / (2*const.hbar)
 
-    transitions_3_to_2 = Transitions(n=2, n_prime=1)
+    transitions_3_to_2 = Transitions(n=3, n_prime=2)
     print(transitions_3_to_2)
+    transitions_3_to_2_rs_mean = transitions_3_to_2.get_spontanious_decay_mean() / rs_mean_normalized_coeff
     print(f"R^s_mean / rs_mean_normalized_coeff  ="
-          f" {transitions_3_to_2.get_spontanious_decay_mean() / rs_mean_normalized_coeff}")
+          f" {transitions_3_to_2_rs_mean}")
     print(f"omega / omega_normalized_coeff  ="
           f" {transitions_3_to_2.get_angular_frequency(3, 2) / omega_normalized_coeff}")
