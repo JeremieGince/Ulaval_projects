69c54c9ed3099a3eefc013200d2f0c8b0cdc1731
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2019-12-07 21:37:58 -0500

    main

--- PHY-2007_physique_atomique/Projet/main.py
+++ PHY-2007_physique_atomique/Projet/main.py
@@ -1,15 +1,23 @@
 from Transitions import Transitions
 import Constantes as const
+import numpy as np
 
 
 if __name__ == '__main__':
-    rs_mean_normalized_coeff = ((const.alpha**5)*const.mu0*(const.c**2))/const.hbar
-    omega_normalized_coeff = ((const.alpha ** 2) * const.mu0 * (const.c ** 2)) / (2*const.hbar)
+    rs_mean_normalized_coeff = ((const.alpha**5)*const.mu_H*(const.c**2))/const.hbar
+    omega_normalized_coeff = ((const.alpha ** 2) * const.mu_H * (const.c ** 2)) / (2*const.hbar)
 
-    transitions_3_to_2 = Transitions(n=3, n_prime=2)
-    print(transitions_3_to_2)
-    transitions_3_to_2_rs_mean = transitions_3_to_2.get_spontanious_decay_mean() / rs_mean_normalized_coeff
+    n, n_prime = 2, 1
+
+    transitions_n_to_n_prime = Transitions(n=n, n_prime=n_prime)
+    print(transitions_n_to_n_prime)
+
+    transitions_n_to_n_prime_rs_mean = transitions_n_to_n_prime.get_spontanious_decay_mean() / rs_mean_normalized_coeff
     print(f"R^s_mean / rs_mean_normalized_coeff  ="
-          f" {transitions_3_to_2_rs_mean}")
+          f" {transitions_n_to_n_prime_rs_mean}")
+    reel_rs_3_to_2_mean = 0.00274668657777777777777777777778
+    print(f"reel R^s = {reel_rs_3_to_2_mean},"
+          f" deviation: {100 * (np.abs(transitions_n_to_n_prime_rs_mean - reel_rs_3_to_2_mean) / rs_mean_normalized_coeff)} %")
+
     print(f"omega / omega_normalized_coeff  ="
-          f" {transitions_3_to_2.get_angular_frequency(3, 2) / omega_normalized_coeff}")
+          f" {transitions_n_to_n_prime.get_angular_frequency(3, 2, const.Z_H, const.mu_H) / omega_normalized_coeff}")
