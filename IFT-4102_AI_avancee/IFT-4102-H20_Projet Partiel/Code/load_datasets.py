d645dadee8032253506c65605f29c446448bbff5
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-01-24 18:06:13 -0500

    Update load_datasets.py

--- IFT-4102_AI_avancee/IFT-4102-H20_Projet Partiel/Code/load_datasets.py
+++ IFT-4102_AI_avancee/IFT-4102-H20_Projet Partiel/Code/load_datasets.py
@@ -1,7 +1,8 @@
 import numpy as np
 import random
 
-def load_iris_dataset(train_ratio):
+
+def load_iris_dataset(train_ratio: float) -> tuple:
     """Cette fonction a pour but de lire le dataset Iris
 
     Args:
@@ -12,45 +13,49 @@ def load_iris_dataset(train_ratio):
 
     Retours:
         Cette fonction doit retourner 4 matrices de type Numpy, train, train_labels, test, et test_labels
-		
-        - train : une matrice numpy qui contient les exemples qui vont etre utilisés pour l'entrainement, chaque 
+
+        - train : une matrice numpy qui contient les exemples qui vont etre utilisés pour l'entrainement, chaque
         ligne dans cette matrice représente un exemple (ou instance) d'entrainement.
-		
+
         - train_labels : contient les labels (ou les étiquettes) pour chaque exemple dans train, de telle sorte
           que : train_labels[i] est le label (ou l'etiquette) pour l'exemple train[i]
-        
-        - test : une matrice numpy qui contient les exemples qui vont etre utilisés pour le test, chaque 
+
+        - test : une matrice numpy qui contient les exemples qui vont etre utilisés pour le test, chaque
         ligne dans cette matrice représente un exemple (ou instance) de test.
-		
+
         - test_labels : contient les labels (ou les étiquettes) pour chaque exemple dans test, de telle sorte
           que : test_labels[i] est le label (ou l'etiquette) pour l'exemple test[i]
     """
-    
-    random.seed(1) # Pour avoir les meme nombres aléatoires à chaque initialisation.
-    
+
+    random.seed(1)  # Pour avoir les meme nombres aléatoires à chaque initialisation.
+
     # Vous pouvez utiliser des valeurs numériques pour les différents types de classes, tel que :
-    conversion_labels = {'Iris-setosa': 0, 'Iris-versicolor' : 1, 'Iris-virginica' : 2}
-    
-    # Le fichier du dataset est dans le dossier datasets en attaché 
-    f = open('datasets/bezdekIris.data', 'r')
-    
-    
-    # TODO : le code ici pour lire le dataset
-    
-    # REMARQUE très importante : 
-	# remarquez bien comment les exemples sont ordonnés dans 
-    # le fichier du dataset, ils sont ordonnés par type de fleur, cela veut dire que 
-    # si vous lisez les exemples dans cet ordre et que si par exemple votre ration est de 60%,
-    # vous n'allez avoir aucun exemple du type Iris-virginica pour l'entrainement, pensez
-    # donc à utiliser la fonction random.shuffle pour melanger les exemples du dataset avant de séparer
-    # en train et test.
-       
-    
-    # Tres important : la fonction doit retourner 4 matrices (ou vecteurs) de type Numpy. 
-    return (train, train_labels, test, test_labels)
-	
-	
-	
+    conversion_labels = {'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2}
+
+    data: list = list()
+    data_labels: list = list()
+    with open('datasets/bezdekIris.data', 'r') as file:
+        lines: list = file.readlines()
+        random.shuffle(lines)
+        for line in lines:
+            line_vectorized: list = line.strip().split(',')
+            if line_vectorized and line_vectorized[-1] in conversion_labels:
+                flower_class: int = conversion_labels[line_vectorized[-1]]
+                train_data: list = [float(i) for i in line_vectorized[:-1]]
+                data.append(train_data)
+                data_labels.append(flower_class)
+
+    split_idx: int = int(len(data) * train_ratio)
+
+    train: np.ndarray = np.array(data[:split_idx])
+    train_labels: np.ndarray = np.array(data_labels[:split_idx])
+
+    test: np.ndarray = np.array(data[split_idx:])
+    test_labels: np.ndarray = np.array(data_labels[split_idx:])
+
+    return train, train_labels, test, test_labels
+
+
 def load_congressional_dataset(train_ratio):
     """Cette fonction a pour but de lire le dataset Congressional Voting Records
 
