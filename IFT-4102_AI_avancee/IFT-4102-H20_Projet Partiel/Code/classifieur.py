952180f3f4793881c0001074a41568e4f0f157e9
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-01-26 16:47:27 -0500

    Update classifieur.py

--- IFT-4102_AI_avancee/IFT-4102-H20_Projet Partiel/Code/classifieur.py
+++ IFT-4102_AI_avancee/IFT-4102-H20_Projet Partiel/Code/classifieur.py
@@ -1,9 +1,9 @@
 """
 Vous allez definir une classe pour chaque algorithme que vous allez développer,
 votre classe doit contenit au moins les 3 methodes definies ici bas, 
-	* train 	: pour entrainer le modèle sur l'ensemble d'entrainement
-	* predict 	: pour prédire la classe d'un exemple donné
-	* test 		: pour tester sur l'ensemble de test
+    * train 	: pour entrainer le modèle sur l'ensemble d'entrainement
+    * predict 	: pour prédire la classe d'un exemple donné
+    * test 		: pour tester sur l'ensemble de test
 vous pouvez rajouter d'autres méthodes qui peuvent vous etre utiles, mais moi
 je vais avoir besoin de tester les méthodes test, predict et test de votre code.
 """
@@ -19,74 +19,86 @@ class Classifier:
 
     def __init__(self, **kwargs):
         """
-        c'est un Initializer.
-        Vous pouvez passer d'autre paramètres au besoin,
-        c'est à vous d'utiliser vos propres notations
-        """
+		c'est un Initializer.
+		Vous pouvez passer d'autre paramètres au besoin,
+		c'est à vous d'utiliser vos propres notations
+		"""
         pass
 
-    def train(self, train, train_labels):
+    def train(self, train, train_labels, verbose: bool = True, findBestKWithCrossValidation: bool = False):
         """
-        c'est la méthode qui va entrainer votre modèle,
-        train est une matrice de type Numpy et de taille nxm, avec
-        n : le nombre d'exemple d'entrainement dans le dataset
-        m : le mobre d'attribus (le nombre de caractéristiques)
+		c'est la méthode qui va entrainer votre modèle,
+		train est une matrice de type Numpy et de taille nxm, avec
+		n : le nombre d'exemple d'entrainement dans le dataset
+		m : le mobre d'attribus (le nombre de caractéristiques)
 
-        train_labels : est une matrice numpy de taille nx1
+		train_labels : est une matrice numpy de taille nx1
 
-        vous pouvez rajouter d'autres arguments, il suffit juste de
-        les expliquer en commentaire
+		vous pouvez rajouter d'autres arguments, il suffit juste de
+		les expliquer en commentaire
 
 
 
-        ------------
-        Après avoir fait l'entrainement, faites maintenant le test sur
-        les données d'entrainement
-        IMPORTANT :
-        Vous devez afficher ici avec la commande print() de python,
-        - la matrice de confision (confusion matrix)
-        - l'accuracy
-        - la précision (precision)
-        - le rappel (recall)
+		------------
+		Après avoir fait l'entrainement, faites maintenant le test sur
+		les données d'entrainement
+		IMPORTANT :
+		Vous devez afficher ici avec la commande print() de python,
+		- la matrice de confision (confusion matrix)
+		- l'accuracy
+		- la précision (precision)
+		- le rappel (recall)
 
-        Bien entendu ces tests doivent etre faits sur les données d'entrainement
-        nous allons faire d'autres tests sur les données de test dans la méthode test()
-        """
+		Bien entendu ces tests doivent etre faits sur les données d'entrainement
+		nous allons faire d'autres tests sur les données de test dans la méthode test()
+		"""
+
         raise NotImplementedError()
 
     def predict(self, exemple, label):
         """
-        Prédire la classe d'un exemple donné en entrée
-        exemple est de taille 1xm
+		Prédire la classe d'un exemple donné en entrée
+		exemple est de taille 1xm
 
-        si la valeur retournée est la meme que la veleur dans label
-        alors l'exemple est bien classifié, si non c'est une missclassification
+		si la valeur retournée est la meme que la veleur dans label
+		alors l'exemple est bien classifié, si non c'est une missclassification
 
-        """
+		"""
         raise NotImplementedError()
 
-    def test(self, test, test_labels):
+    def test(self, test, test_labels, verbose: bool = True, displayArgs: dict = None):
         """
-        c'est la méthode qui va tester votre modèle sur les données de test
-        l'argument test est une matrice de type Numpy et de taille nxm, avec
-        n : le nombre d'exemple de test dans le dataset
-        m : le mobre d'attribus (le nombre de caractéristiques)
+		c'est la méthode qui va tester votre modèle sur les données de test
+		l'argument test est une matrice de type Numpy et de taille nxm, avec
+		n : le nombre d'exemple de test dans le dataset
+		m : le mobre d'attribus (le nombre de caractéristiques)
 
-        test_labels : est une matrice numpy de taille nx1
+		test_labels : est une matrice numpy de taille nx1
 
-        vous pouvez rajouter d'autres arguments, il suffit juste de
-        les expliquer en commentaire
+		vous pouvez rajouter d'autres arguments, il suffit juste de
+		les expliquer en commentaire
 
-        Faites le test sur les données de test, et afficher :
-        - la matrice de confision (confusion matrix)
-        - l'accuracy
-        - la précision (precision)
-        - le rappel (recall)
+		Faites le test sur les données de test, et afficher :
+		- la matrice de confision (confusion matrix)
+		- l'accuracy
+		- la précision (precision)
+		- le rappel (recall)
 
-        Bien entendu ces tests doivent etre faits sur les données de test seulement
+		Bien entendu ces tests doivent etre faits sur les données de test seulement
 
-        """
-        raise NotImplementedError()
+		"""
+        confusionMatrix: np.ndarray = self.getConfusionMatrix(test, test_labels)
+        accuracy: float = self.getAccuracy(test, test_labels)
+        precision = self.getPrecision(test, test_labels)
+        recall = self.getRecall(test, test_labels)
+
+        if verbose:
+            if displayArgs is None:
+                displayArgs = {"dataSize": len(test), "title": "Test results", "preMessage": ""}
+            self.displayStats(confusionMatrix, accuracy, precision, recall, dataSize=displayArgs["dataSize"],
+                              title=displayArgs["title"], preMessage=displayArgs["preMessage"])
+
+        return confusionMatrix, accuracy, precision, recall
 
     def getAccuracy(self, test: np.ndarray, test_labels: np.ndarray) -> float:
         accuracy: float = 0
@@ -123,8 +135,8 @@ class Classifier:
     def displayStats(self, confusionMatrix: np.ndarray = None, accuracy: float = None, precision: float = None,
                      recall: float = None, dataSize: int = None, title: str = "", preMessage: str = ""):
         print((f"\n {title}:" if title else ""),
-              f"Test set size: {dataSize}",
-			  (f"{preMessage}" if preMessage else ""),
+              f"Data set size: {dataSize}",
+              (f"{preMessage}" if preMessage else ""),
               f"Confusion Matrix: \n {confusionMatrix}",
               f"Accuracy: {accuracy:.2f} %",
               f"Precision: {precision:.5f}",
