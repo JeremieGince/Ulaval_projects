21775557081102bbdf14bbca7e0558907b6da6b0
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-01-26 15:06:08 -0500

    Update classifieur.py

--- IFT-4102_AI_avancee/IFT-4102-H20_Projet Partiel/Code/classifieur.py
+++ IFT-4102_AI_avancee/IFT-4102-H20_Projet Partiel/Code/classifieur.py
@@ -17,105 +17,116 @@ import numpy as np
 
 class Classifier:
 
-	def __init__(self, **kwargs):
-		"""
-		c'est un Initializer. 
-		Vous pouvez passer d'autre paramètres au besoin,
-		c'est à vous d'utiliser vos propres notations
-		"""
-		pass
-		
-	def train(self, train, train_labels):
-		"""
-		c'est la méthode qui va entrainer votre modèle,
-		train est une matrice de type Numpy et de taille nxm, avec 
-		n : le nombre d'exemple d'entrainement dans le dataset
-		m : le mobre d'attribus (le nombre de caractéristiques)
-		
-		train_labels : est une matrice numpy de taille nx1
-		
-		vous pouvez rajouter d'autres arguments, il suffit juste de
-		les expliquer en commentaire
-		
-		
-		
-		------------
-		Après avoir fait l'entrainement, faites maintenant le test sur 
-		les données d'entrainement
-		IMPORTANT : 
-		Vous devez afficher ici avec la commande print() de python,
-		- la matrice de confision (confusion matrix)
-		- l'accuracy
-		- la précision (precision)
-		- le rappel (recall)
-		
-		Bien entendu ces tests doivent etre faits sur les données d'entrainement
-		nous allons faire d'autres tests sur les données de test dans la méthode test()
-		"""
-		raise NotImplementedError()
+    def __init__(self, **kwargs):
+        """
+        c'est un Initializer.
+        Vous pouvez passer d'autre paramètres au besoin,
+        c'est à vous d'utiliser vos propres notations
+        """
+        pass
 
-	def predict(self, exemple, label):
-		"""
-		Prédire la classe d'un exemple donné en entrée
-		exemple est de taille 1xm
-		
-		si la valeur retournée est la meme que la veleur dans label
-		alors l'exemple est bien classifié, si non c'est une missclassification
+    def train(self, train, train_labels):
+        """
+        c'est la méthode qui va entrainer votre modèle,
+        train est une matrice de type Numpy et de taille nxm, avec
+        n : le nombre d'exemple d'entrainement dans le dataset
+        m : le mobre d'attribus (le nombre de caractéristiques)
 
-		"""
-		raise NotImplementedError()
+        train_labels : est une matrice numpy de taille nx1
 
-	def test(self, test, test_labels):
-		"""
-		c'est la méthode qui va tester votre modèle sur les données de test
-		l'argument test est une matrice de type Numpy et de taille nxm, avec 
-		n : le nombre d'exemple de test dans le dataset
-		m : le mobre d'attribus (le nombre de caractéristiques)
-		
-		test_labels : est une matrice numpy de taille nx1
-		
-		vous pouvez rajouter d'autres arguments, il suffit juste de
-		les expliquer en commentaire
-		
-		Faites le test sur les données de test, et afficher :
-		- la matrice de confision (confusion matrix)
-		- l'accuracy
-		- la précision (precision)
-		- le rappel (recall)
-		
-		Bien entendu ces tests doivent etre faits sur les données de test seulement
-		
-		"""
-		raise NotImplementedError()
+        vous pouvez rajouter d'autres arguments, il suffit juste de
+        les expliquer en commentaire
 
-	def getAccuracy(self, test: np.ndarray, test_labels: np.ndarray) -> float:
-		accuracy: float = 0
-		for idx, exemple in enumerate(test):
-			prediction, check = self.predict(exemple, test_labels[idx])
-			accuracy += int(check)
-		return 100 * (accuracy / len(test))
 
-	def getConfusionMatrix(self, test: np.ndarray, test_labels: np.ndarray) -> np.ndarray:
-		# https://en.wikipedia.org/wiki/Confusion_matrix
-		labels: list = sorted(list(set(test_labels)))
-		labelsToCountclassification: dict = {lbl: [0 for _ in labels] for lbl in labels}
-		for idx, example in enumerate(test):
-			prediction, check = self.predict(example, test_labels[idx])
-			labelsToCountclassification[test_labels[idx]][labels.index(prediction)] += 1
 
-		confusionMatrix: np.ndarray = np.array([labelsToCountclassification[lbl] for lbl in labels]).transpose()
-		return confusionMatrix
+        ------------
+        Après avoir fait l'entrainement, faites maintenant le test sur
+        les données d'entrainement
+        IMPORTANT :
+        Vous devez afficher ici avec la commande print() de python,
+        - la matrice de confision (confusion matrix)
+        - l'accuracy
+        - la précision (precision)
+        - le rappel (recall)
 
-	def getPrecision(self, test: np.ndarray, test_labels: np.ndarray) -> np.float:
-		# https://fr.wikipedia.org/wiki/Pr%C3%A9cision_et_rappel
-		confusionMatrix: np.ndarray = self.getConfusionMatrix(test, test_labels)
-		precisionVector: np.ndarray = np.array([vector[idx]/np.sum(vector)
-												for idx, vector in enumerate(confusionMatrix)])
-		return precisionVector.mean()
+        Bien entendu ces tests doivent etre faits sur les données d'entrainement
+        nous allons faire d'autres tests sur les données de test dans la méthode test()
+        """
+        raise NotImplementedError()
 
-	def getRecall(self, test: np.ndarray, test_labels: np.ndarray) -> np.float:
-		# https://fr.wikipedia.org/wiki/Pr%C3%A9cision_et_rappel
-		confusionMatrix_T: np.ndarray = self.getConfusionMatrix(test, test_labels).transpose()
-		recallVector: np.ndarray = np.array([vector[idx]/np.sum(vector)
-												for idx, vector in enumerate(confusionMatrix_T)])
-		return recallVector.mean()
+    def predict(self, exemple, label):
+        """
+        Prédire la classe d'un exemple donné en entrée
+        exemple est de taille 1xm
+
+        si la valeur retournée est la meme que la veleur dans label
+        alors l'exemple est bien classifié, si non c'est une missclassification
+
+        """
+        raise NotImplementedError()
+
+    def test(self, test, test_labels):
+        """
+        c'est la méthode qui va tester votre modèle sur les données de test
+        l'argument test est une matrice de type Numpy et de taille nxm, avec
+        n : le nombre d'exemple de test dans le dataset
+        m : le mobre d'attribus (le nombre de caractéristiques)
+
+        test_labels : est une matrice numpy de taille nx1
+
+        vous pouvez rajouter d'autres arguments, il suffit juste de
+        les expliquer en commentaire
+
+        Faites le test sur les données de test, et afficher :
+        - la matrice de confision (confusion matrix)
+        - l'accuracy
+        - la précision (precision)
+        - le rappel (recall)
+
+        Bien entendu ces tests doivent etre faits sur les données de test seulement
+
+        """
+        raise NotImplementedError()
+
+    def getAccuracy(self, test: np.ndarray, test_labels: np.ndarray) -> float:
+        accuracy: float = 0
+        for idx, exemple in enumerate(test):
+            prediction, check = self.predict(exemple, test_labels[idx])
+            accuracy += int(check)
+        return 100 * (accuracy / len(test))
+
+    def getConfusionMatrix(self, test: np.ndarray, test_labels: np.ndarray) -> np.ndarray:
+        # https://en.wikipedia.org/wiki/Confusion_matrix
+        labels: list = sorted(list(set(test_labels)))
+        labelsToCountclassification: dict = {lbl: [0 for _ in labels] for lbl in labels}
+        for idx, example in enumerate(test):
+            prediction, check = self.predict(example, test_labels[idx])
+            labelsToCountclassification[test_labels[idx]][labels.index(prediction)] += 1
+
+        confusionMatrix: np.ndarray = np.array([labelsToCountclassification[lbl] for lbl in labels]).transpose()
+        return confusionMatrix
+
+    def getPrecision(self, test: np.ndarray, test_labels: np.ndarray) -> np.float:
+        # https://fr.wikipedia.org/wiki/Pr%C3%A9cision_et_rappel
+        confusionMatrix: np.ndarray = self.getConfusionMatrix(test, test_labels)
+        precisionVector: np.ndarray = np.array([vector[idx] / np.sum(vector)
+                                                for idx, vector in enumerate(confusionMatrix)])
+        return precisionVector.mean()
+
+    def getRecall(self, test: np.ndarray, test_labels: np.ndarray) -> np.float:
+        # https://fr.wikipedia.org/wiki/Pr%C3%A9cision_et_rappel
+        confusionMatrix_T: np.ndarray = self.getConfusionMatrix(test, test_labels).transpose()
+        recallVector: np.ndarray = np.array([vector[idx] / np.sum(vector)
+                                             for idx, vector in enumerate(confusionMatrix_T)])
+        return recallVector.mean()
+
+    def displayStats(self, confusionMatrix: np.ndarray = None, accuracy: float = None, precision: float = None,
+                     recall: float = None, dataSize: int = None, title: str = "", preMessage: str = ""):
+        print((f"\n {title}:" if title else ""),
+              f"Test set size: {dataSize}",
+			  (f"{preMessage}" if preMessage else ""),
+              f"Confusion Matrix: \n {confusionMatrix}",
+              f"Accuracy: {accuracy:.2f} %",
+              f"Precision: {precision:.5f}",
+              f"Recall: {recall:.5f}",
+              sep='\n')
