a8152a7a747fe769138c615426999697e070d5f7
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-01-26 17:57:51 -0500

    Doc

--- IFT-4102_AI_avancee/IFT-4102-H20_Projet Partiel/Code/classifieur.py
+++ IFT-4102_AI_avancee/IFT-4102-H20_Projet Partiel/Code/classifieur.py
@@ -16,6 +16,9 @@ import numpy as np
 # Knn pour le modèle des k plus proches voisins
 
 class Classifier:
+    """
+	Parent class for every classifier. Abstract class.
+	"""
 
     def __init__(self, **kwargs):
         """
@@ -25,43 +28,29 @@ class Classifier:
 		"""
         pass
 
-    def train(self, train, train_labels, verbose: bool = True, findBestKWithCrossValidation: bool = False):
+    def train(self, train: np.ndarray, train_labels: np.ndarray,
+			  verbose: bool = True, **kwargs):
         """
-		c'est la méthode qui va entrainer votre modèle,
-		train est une matrice de type Numpy et de taille nxm, avec
+		Used to train the current model.
+
+		:param train: une matrice de type Numpy et de taille nxm, avec
 		n : le nombre d'exemple d'entrainement dans le dataset
 		m : le mobre d'attribus (le nombre de caractéristiques)
 
-		train_labels : est une matrice numpy de taille nx1
-
-		vous pouvez rajouter d'autres arguments, il suffit juste de
-		les expliquer en commentaire
-
-
-
-		------------
-		Après avoir fait l'entrainement, faites maintenant le test sur
-		les données d'entrainement
-		IMPORTANT :
-		Vous devez afficher ici avec la commande print() de python,
-		- la matrice de confision (confusion matrix)
-		- l'accuracy
-		- la précision (precision)
-		- le rappel (recall)
-
-		Bien entendu ces tests doivent etre faits sur les données d'entrainement
-		nous allons faire d'autres tests sur les données de test dans la méthode test()
+		:param train_labels : est une matrice numpy de taille nx1
+		:param verbose: True To print results else False. (bool)
+		:param kwargs: Parameters to pass to child classes.
 		"""
 
         raise NotImplementedError()
 
-    def predict(self, exemple, label):
+    def predict(self, exemple, label) -> tuple:
         """
 		Prédire la classe d'un exemple donné en entrée
-		exemple est de taille 1xm
+		:param exemple: Matrice de taille 1xm
+		:param label: The label of the example.
 
-		si la valeur retournée est la meme que la veleur dans label
-		alors l'exemple est bien classifié, si non c'est une missclassification
+		:return (prediction, prediction == label). (tuple)
 
 		"""
         raise NotImplementedError()
