63628dea64e29d4a98e3a4256590fd45256903c5
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-03-15 09:22:36 -0400

    Update entrainer_tester.py

--- IFT-4102_AI_avancee/IFT-4102-H20_Projet Partiel/Code/entrainer_tester.py
+++ IFT-4102_AI_avancee/IFT-4102-H20_Projet Partiel/Code/entrainer_tester.py
@@ -1,9 +1,9 @@
-import numpy as np
-import sys, time
-import load_datasets
-from Knn import Knn
-from BayesNaif import Nbc, NbcGaussian
+import time
 
+import load_datasets
+import util
+from BayesNaif import Nbc, NbcGaussian
+from Knn import Knn
 
 """
 C'est le fichier main duquel nous allons tout lancer
@@ -16,98 +16,99 @@ En gros, vous allez :
 
 """
 
-#######################################################################################################################
-#  Partie 1 - Knn
-#######################################################################################################################
+if __name__ == '__main__':
+    ###################################################################################################################
+    #  Partie 1 - Knn
+    ###################################################################################################################
 
-knn_train_ratio: float = 0.9
-findBestKWithCrossValidation: bool = True
+    knn_train_ratio: float = 0.9  # Ici, on initialise train ratio pour le Knn
+    findBestKWithCrossValidation: bool = True  # Ici, on veut faire de la cross validation afin d'optimiser nos K
+    distanceFunc = util.euclidean_distance  # On choisi notre métric de distance comme étant la distance euclidienne
 
-print(f"Knn Train ratio: {knn_train_ratio}")
-print(f"findBestKWithCrossValidation: {findBestKWithCrossValidation}")
-print("\n")
+    print(f"Knn Train ratio: {knn_train_ratio}")
+    print(f"findBestKWithCrossValidation: {findBestKWithCrossValidation}")
+    print("\n")
 
-print('-' * 175)
-print(f"Iris dataset classification: \n")
-startTime = time.time()
-
-iris_train, iris_train_labels, iris_test, iris_test_labels = load_datasets.load_iris_dataset(knn_train_ratio)
-iris_knn = Knn()
-iris_knn.train(iris_train, iris_train_labels, findBestKWithCrossValidation=findBestKWithCrossValidation)
-iris_knn.test(iris_test, iris_test_labels)
-
-print(f"\n --- Elapse time: {time.time() - startTime:.2f} s --- \n")
-
-print('-' * 175)
-print(f"Congressional dataset classification: \n")
-startTime = time.time()
-
-cong_train, cong_train_labels, cong_test, cong_test_labels = load_datasets.load_congressional_dataset(knn_train_ratio)
-cong_knn = Knn()
-cong_knn.train(cong_train, cong_train_labels, findBestKWithCrossValidation=findBestKWithCrossValidation)
-cong_knn.test(cong_test, cong_test_labels)
-
-print(f"\n --- Elapse time: {time.time() - startTime:.2f} s --- \n")
-
-print('-' * 175)
-for i in range(3):
-    print(f"Monks({i + 1}) dataset classification: \n")
+    print('-' * 175)
+    print(f"Iris dataset classification: \n")
     startTime = time.time()
 
-    monks_train, monks_train_labels, monks_test, monks_test_labels = load_datasets.load_monks_dataset(i + 1)
-    monks_knn = Knn()
-    monks_knn.train(monks_train, monks_train_labels, findBestKWithCrossValidation=findBestKWithCrossValidation)
-    monks_knn.test(monks_test, monks_test_labels)
+    iris_train, iris_train_labels, iris_test, iris_test_labels = load_datasets.load_iris_dataset(knn_train_ratio)
+    iris_knn = Knn(distance_func=distanceFunc)
+    iris_knn.train(iris_train, iris_train_labels, findBestKWithCrossValidation=findBestKWithCrossValidation)
+    iris_knn.test(iris_test, iris_test_labels)
 
     print(f"\n --- Elapse time: {time.time() - startTime:.2f} s --- \n")
 
     print('-' * 175)
-
-
-#######################################################################################################################
-#  Partie 2 - Bayes Naif
-#######################################################################################################################
-
-nbc_train_ratio: float = 0.4
-
-print(f"Bayes Naif Train ratio: {nbc_train_ratio}")
-print("\n")
-
-print('-' * 175)
-print(f"Iris dataset classification: \n")
-startTime = time.time()
-
-iris_train, iris_train_labels, iris_test, iris_test_labels = load_datasets.load_iris_dataset(nbc_train_ratio)
-iris_knn = NbcGaussian()
-
-iris_knn.train(iris_train, iris_train_labels)
-iris_knn.test(iris_test, iris_test_labels)
-
-print(f"\n --- Elapse time: {time.time() - startTime:.2f} s --- \n")
-
-print('-' * 175)
-print(f"Congressional dataset classification: \n")
-startTime = time.time()
-
-cong_train, cong_train_labels, cong_test, cong_test_labels = load_datasets.load_congressional_dataset(nbc_train_ratio)
-cong_knn = Nbc()
-cong_knn.train(cong_train, cong_train_labels)
-cong_knn.test(cong_test, cong_test_labels)
-
-print(f"\n --- Elapse time: {time.time() - startTime:.2f} s --- \n")
-
-print('-' * 175)
-for i in range(3):
-    print(f"Monks({i + 1}) dataset classification: \n")
+    print(f"Congressional dataset classification: \n")
     startTime = time.time()
 
-    monks_train, monks_train_labels, monks_test, monks_test_labels = load_datasets.load_monks_dataset(i + 1)
-    monks_knn = Nbc()
-    monks_knn.train(monks_train, monks_train_labels)
-    monks_knn.test(monks_test, monks_test_labels)
+    cong_train, cong_train_labels, cong_test, cong_test_labels = load_datasets.load_congressional_dataset(
+        knn_train_ratio)
+    cong_knn = Knn(distance_func=distanceFunc)
+    cong_knn.train(cong_train, cong_train_labels, findBestKWithCrossValidation=findBestKWithCrossValidation)
+    cong_knn.test(cong_test, cong_test_labels)
 
     print(f"\n --- Elapse time: {time.time() - startTime:.2f} s --- \n")
 
     print('-' * 175)
+    for i in range(3):
+        print(f"Monks({i + 1}) dataset classification: \n")
+        startTime = time.time()
 
+        monks_train, monks_train_labels, monks_test, monks_test_labels = load_datasets.load_monks_dataset(i + 1)
+        monks_knn = Knn(distance_func=distanceFunc)
+        monks_knn.train(monks_train, monks_train_labels, findBestKWithCrossValidation=findBestKWithCrossValidation)
+        monks_knn.test(monks_test, monks_test_labels)
 
+        print(f"\n --- Elapse time: {time.time() - startTime:.2f} s --- \n")
+
+        print('-' * 175)
+
+    ###################################################################################################################
+    #  Partie 2 - Bayes Naif
+    ###################################################################################################################
+
+    nbc_train_ratio: float = 0.4
+
+    print(f"Bayes Naif Train ratio: {nbc_train_ratio}")
+    print("\n")
+
+    print('-' * 175)
+    print(f"Iris dataset classification: \n")
+    startTime = time.time()
+
+    iris_train, iris_train_labels, iris_test, iris_test_labels = load_datasets.load_iris_dataset(nbc_train_ratio)
+    iris_knn = NbcGaussian()
+
+    iris_knn.train(iris_train, iris_train_labels)
+    iris_knn.test(iris_test, iris_test_labels)
+
+    print(f"\n --- Elapse time: {time.time() - startTime:.2f} s --- \n")
+
+    print('-' * 175)
+    print(f"Congressional dataset classification: \n")
+    startTime = time.time()
+
+    cong_train, cong_train_labels, cong_test, cong_test_labels = load_datasets.load_congressional_dataset(
+        nbc_train_ratio)
+    cong_knn = Nbc()
+    cong_knn.train(cong_train, cong_train_labels)
+    cong_knn.test(cong_test, cong_test_labels)
+
+    print(f"\n --- Elapse time: {time.time() - startTime:.2f} s --- \n")
+
+    print('-' * 175)
+    for i in range(3):
+        print(f"Monks({i + 1}) dataset classification: \n")
+        startTime = time.time()
+
+        monks_train, monks_train_labels, monks_test, monks_test_labels = load_datasets.load_monks_dataset(i + 1)
+        monks_knn = Nbc()
+        monks_knn.train(monks_train, monks_train_labels)
+        monks_knn.test(monks_test, monks_test_labels)
+
+        print(f"\n --- Elapse time: {time.time() - startTime:.2f} s --- \n")
+
+        print('-' * 175)
